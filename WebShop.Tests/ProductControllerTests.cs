fae575b5455e47a64647fbf1025b77409c50dae1
Kevin Sahlén <143435191+SahlenKevin@users.noreply.github.com>
2024-11-19 12:11:32 +0100

    AddProduct_ReturnsOkResultTest Is Green

--- WebShop.Tests/ProductControllerTests.cs
+++ WebShop.Tests/ProductControllerTests.cs
@@ -1,19 +1,18 @@
 using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
 using WebShop;
 using WebShop.Controllers;
-using WebShop.Repositories;
 using WebShop.UnitOfWork;
 
 public class ProductControllerTests
 {
-    private readonly IProductRepository _mockProductRepository;
     private readonly ProductController _controller;
+    private readonly IUnitOfWork _unitOfWork;
 
     public ProductControllerTests()
     {
-        var unitOfWork = A.Fake<IUnitOfWork>();
-        _mockProductRepository = A.Fake<IProductRepository>();
-        _controller = new ProductController(unitOfWork);
+        _unitOfWork = A.Fake<IUnitOfWork>();
+        _controller = new ProductController(_unitOfWork);
         // Ställ in mock av Products-egenskapen
     }
 
@@ -22,7 +21,7 @@ public class ProductControllerTests
     {
         // Arrange
         List<Product> products = (List<Product>)A.CollectionOfDummy<Product>(1);
-
+        
         // Act
         var result = _controller.GetProducts();
         
@@ -34,9 +33,15 @@ public class ProductControllerTests
     public void AddProduct_ReturnsOkResult()
     {
         // Arrange
-
+        var testProduct = A.Fake<Product>();
+        A.CallTo(() => _unitOfWork.Products.Add(testProduct)).DoesNothing();    
+        
         // Act
-
+        var result = _controller.AddProduct(testProduct);
+        
         // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(200, okResult.StatusCode);
+        A.CallTo(() => _unitOfWork.Products.Add(testProduct)).MustHaveHappenedOnceExactly();
     }
 }
